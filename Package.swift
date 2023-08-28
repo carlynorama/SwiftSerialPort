32281508c89eebf2dea2b134e7ac3efc5e56bb44
carlynorama <carlynorama@gmail.com>
2023-08-28 10:46:21 -0700

    update package

--- Package.swift
+++ Package.swift
@@ -12,14 +12,17 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftSerialPort",
-            targets: ["SwiftSerialPort", "SerialC"]),
+            targets: ["SwiftSerialPort"]),
+        .library(
+            name: "SerialC",
+            targets: ["SerialC"]),
         .executable(name: "cereal", targets: ["cereal"])
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SwiftSerialPort"),
+            name: "SwiftSerialPort", dependencies: ["SerialC"]),
         .target(
             name: "SerialC"),
         .executableTarget(name: "cereal", dependencies: ["SwiftSerialPort", "SerialC"]),
